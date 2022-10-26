7f5515813bcfe1a08d75ea812ed2cd7106cd816b
Derek Ludlow <lud15002@byui.edu>
2022-10-26 07:47:20 -0600

    update new folder

---
+++ week 2/wednesday.js
@@ -0,0 +1,8 @@
+function changetext() { 
+    document.getEelementById('divWithText').innerHTML = 'hi there' ;
+    let newText = 'hi there' ; 
+    let oldText = 'this is some sweet text in my div' ; 
+    let pageText = document.getElementById('divWithText')
+    
+    
+    }
